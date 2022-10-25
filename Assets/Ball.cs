ca9cc88b9f710bc4b7ff9ba2a416525666dd02fa
Bastien <bastien.reys@neuf.fr>
2022-10-25 22:16:49 +0200

    debug ball

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,19 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
 
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rb.velocity.magnitude < 0.40f) {
+            Debug.Log("Add lil force");
+        }
     }
 }
